eaff3a0252c3c9bd9900a6de7c81f4ef2f6acb6a
GabrielHidaN <victorbielgv7@gmail.com>
2024-12-03 22:38:05 -0300

    add: create page TodoList and yours firsts tasks

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Todo
 
 
 # Create your views here.
 
 def todo_list(request):
-    return render(request , "todos/todo_list.html")
+    todos = Todo.objects.all()
+    return render(request , "todos/todo_list.html",
+                  {"todos": todos})
